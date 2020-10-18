99dc6ec04e0844e78b56a246e2367f89439493ca
Jorge Alonso <jorgealonso7@outlook.es>
2020-10-18 15:25:30 +0200

    Fragmentos finalizados (falta completar los horarios de AyF)

--- proyecto/src/main/java/com/salesianostriana/edu/proyecto/controller/JefeController.java
+++ proyecto/src/main/java/com/salesianostriana/edu/proyecto/controller/JefeController.java
@@ -1,16 +1,22 @@
 package com.salesianostriana.edu.proyecto.controller;
 
+import com.salesianostriana.edu.proyecto.modelo.Profesor;
+import com.salesianostriana.edu.proyecto.servicio.ProfesorServicio;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class JefeController {
 
-    @GetMapping("/jefe/principal")
-    public String jefeEstudios() {
+    private final ProfesorServicio profesorServicio;
 
+    @GetMapping("/jefe/principal")
+    public String jefeEstudios(Model model,  @AuthenticationPrincipal Profesor usuarioLog) {
+        model.addAttribute("usuarioLogeado", profesorServicio.findByEmail(usuarioLog.getEmail()));
         return "jefe/principal";
     }
 
